c0aa0b0abe117585421d17f80c66f1107e3344f3
barbieri <barbieri@0aba3d35-c124-0410-a9e5-bd86f66566d0>
2007-03-08 22:34:37 +0000

    Read items from file, handle keyboard events.

--- efl-tests/text-list/app.c
+++ efl-tests/text-list/app.c
@@ -1,6 +1,8 @@
+#define _GNU_SOURCE
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
 #include <Ecore_Evas.h>
 #include <Ecore.h>
 #include <Edje.h>
@@ -16,11 +18,19 @@
 
 typedef struct app
 {
+    char *infile;
     Evas_Object *edje_main;
     Evas_Object *e_box;
     Ecore_Evas  *ee;
     Evas *evas;
+    int current;
 } app_t;
+
+static inline int
+eq(const char *a, const char *b)
+{
+    return strcmp(a, b) == 0;
+}
 
 static Evas_Object *
 _new_list_item(app_t *app, const char *text)
@@ -37,33 +47,94 @@ _new_list_item(app_t *app, const char *text)
 static void
 _populate(app_t *app)
 {
-    char *t[] = {
-        "(Exchange)",
-        "914",
-        "A Flor",
-        "A Guy Named Sid - Pt. 1",
-        "A Guy Named Sid - Pt. 2",
-        "A Guy Named Sid - Pt. 3",
-        "A Guy Named Sid - Pt. 4",
-        NULL
-    };
-    char buf[512];
-    int i;
+    FILE *fp;
 
-    for (i=0; i < 7; i++) {
+    if (app->infile) {
+        fp = fopen(app->infile, "r");
+        if (fp == NULL) {
+            fprintf(stderr, "Could not open file for reading \"%s\": %s\n",
+                    app->infile, strerror(errno));
+            return;
+        }
+    } else {
+        fprintf(stderr, "No input file provided, reading from stdin.\n");
+        fp = stdin;
+    }
+
+    while (!feof(fp)) {
         Evas_Object *obj;
         Evas_Coord w, h;
+        char *line;
+        unsigned n;
+        int i;
 
-        //sprintf(buf, "Item %d", i);
+        n = 0;
+        line = NULL;
+        i = getline(&line, &n, fp);
+        if (i < 0)
+            break;
 
-        obj = _new_list_item(app, t[i]/* buf */);
+        if (i == 0) {
+            free(line);
+            continue;
+        }
+        line[i - 1] = '\0';
+
+        obj = _new_list_item(app, line);
         e_box_pack_end(app->e_box, obj);
         edje_object_size_min_calc(obj, &w, &h);
         e_box_pack_options_set(obj, 1, 1, 1, 0, 0.0, 0.5, w, h, 9999, h);
         evas_object_show(obj);
+
+        free(line);
     }
 
+    if (fp != stdin)
+        fclose(fp);
+
     e_box_align_set(app->e_box, 0.0, 1.0);
+    app->current = 0;
+}
+
+static void
+move_down(app_t *app)
+{
+    fprintf(stderr, "down\n");
+}
+
+static void
+move_up(app_t *app)
+{
+    fprintf(stderr, "up\n");
+}
+
+static void
+key_down(void *data, Evas *e, Evas_Object *obj, void *event_info)
+{
+    Evas_Event_Key_Down *ev;
+    app_t *app;
+    const char *k;
+
+    ev = (Evas_Event_Key_Down *)event_info;
+    app = (app_t *)data;
+
+    k = ev->keyname;
+
+    if (eq(k, "Down"))
+        move_down(app);
+    else if (eq(k, "Up"))
+        move_up(app);
+    else if (eq(k, "Escape"))
+        ecore_main_loop_quit();
+    else if (eq(k, "f")) {
+        if (ecore_evas_fullscreen_get(app->ee)) {
+            ecore_evas_fullscreen_set(app->ee, 0);
+            ecore_evas_cursor_set(app->ee, NULL, 0, 0, 0);
+        } else {
+            ecore_evas_fullscreen_set(app->ee, 1);
+            ecore_evas_cursor_set(app->ee, " ", 999, 0, 0);
+        }
+    }
 }
 
 static int
@@ -89,7 +160,6 @@ int
 main(int argc, char *argv[])
 {
     app_t app;
-    Evas_Coord edje_w, edje_h;
     int i;
 
     ecore_init();
@@ -109,12 +179,20 @@ main(int argc, char *argv[])
     for (i=1; i < argc; i++)
         if (strcmp (argv[i], "-fs") == 0)
             ecore_evas_fullscreen_set(app.ee, 1);
+        else if (argv[i][0] != '-')
+            app.infile = argv[i];
 
     app.evas = ecore_evas_get(app.ee);
 
     app.edje_main = edje_object_add(app.evas);
     evas_data_attach_set(app.evas, &app);
-    edje_object_file_set(app.edje_main, THEME, THEME_GROUP);
+    if (!edje_object_file_set(app.edje_main, THEME, THEME_GROUP)) {
+        fprintf(stderr, "Failed to load file \"%s\", part \"%s\".\n",
+                THEME, THEME_GROUP);
+        return 1;
+    }
+
+
     evas_object_move(app.edje_main, 0, 0);
     evas_object_resize(app.edje_main, WIDTH, HEIGHT);
 
@@ -133,6 +211,10 @@ main(int argc, char *argv[])
 
     ecore_event_handler_add(ECORE_EVENT_SIGNAL_EXIT, app_signal_exit, NULL);
     ecore_evas_callback_resize_set(app.ee, resize_cb);
+
+    evas_object_event_callback_add(app.edje_main, EVAS_CALLBACK_KEY_DOWN,
+                                   key_down, &app);
+    evas_object_focus_set(app.edje_main, 1);
 
     ecore_main_loop_begin();
 
