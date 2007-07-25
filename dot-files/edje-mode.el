8ac783bfcfed598fba0f1fa61edf844cb13f9a39
barbieri <barbieri@0aba3d35-c124-0410-a9e5-bd86f66566d0>
2007-07-25 18:49:48 +0000

    Improved version

--- dot-files/edje-mode.el
+++ dot-files/edje-mode.el
@@ -41,93 +41,129 @@
 (defconst edje-font-lock-keywords-1
   (eval-when-compile
     (list
-     (list (concat "\\<" (regexp-opt '("action"
-                                       "after"
-                                       "alias"
-                                       "align"
-                                       "angle"
-                                       "aspect"
-                                       "aspect_preference"
-                                       "base"
-                                       "border"
-                                       "clip_to"
-                                       "collections"
-                                       "color"
-                                       "color2"
-                                       "color3"
-                                       "color_class"
-                                       "color_classes"
-                                       "confine"
-                                       "data"
-                                       "description"
-                                       "dragable"
-                                       "effect"
-                                       "elipsis"
-                                       "events"
-                                       "fill"
-                                       "fit"
-                                       "fixed"
-                                       "font"
-                                       "fonts"
-                                       "gradient"
-                                       "group"
-                                       "image"
-                                       "images"
-                                       "in"
-                                       "inherit"
-                                       "item"
-                                       "max"
-                                       "middle"
-                                       "min"
-                                       "mouse_events"
-                                       "name"
-                                       "normal"
-                                       "offset"
-                                       "origin"
-                                       "part"
-                                       "parts"
-                                       "pointer_mode"
-                                       "precise_is_inside"
-                                       "program"
-                                       "programs"
-                                       "rel1"
-                                       "rel2"
-                                       "relative"
-                                       "repeat_events"
-                                       "signal"
-                                       "size"
-                                       "smooth"
-                                       "source"
-                                       "spectra"
-                                       "spectrum"
-                                       "spread"
-                                       "state"
-                                       "step"
-                                       "style"
-                                       "styles"
-                                       "tag"
-                                       "target"
-                                       "text"
-                                       "text_class"
-                                       "text_source"
-                                       "to"
-                                       "to_x"
-                                       "to_y"
-                                       "transition"
-                                       "tween"
-                                       "type"
-                                       "use_alternate_font_metrics"
-                                       "visible"
-                                       "x"
-                                       "y") t) "\\>")
-           '(1 font-lock-keyword-face))
+     (list (concat "[ \t]*\\<"
+                   (regexp-opt
+                    '(
+                      "collections"
+                      "data"
+                      "description"
+                      "fill"
+                      "fonts"
+                      "group"
+                      "image"
+                      "images"
+                      "origin"
+                      "part"
+                      "parts"
+                      "program"
+                      "programs"
+                      "rel1"
+                      "rel2"
+                      "script"
+                      "spectra"
+                      "style"
+                      "styles"
+                      "text"
+                      ) t) "\\>\\([ \t]*{\\|\\.\\)")
+           '(1 font-lock-function-name-face))
 
      ))
-  "Expressions to highlight")
+  "Major keywords")
 
 (defconst edje-font-lock-keywords-2
   (eval-when-compile
     (append edje-font-lock-keywords-1
+            (list
+             (list
+              (concat "^\\([ \t]*\\|[ \t]*[a-z]+\\.\\|\\)\\<"
+                      (regexp-opt
+                       '("action"
+                         "after"
+                         "alias"
+                         "align"
+                         "angle"
+                         "aspect"
+                         "aspect_preference"
+                         "base"
+                         "border"
+                         "clip_to"
+                         "collections"
+                         "color"
+                         "color2"
+                         "color3"
+                         "color_class"
+                         "color_classes"
+                         "confine"
+                         "data"
+                         "description"
+                         "dragable"
+                         "effect"
+                         "elipsis"
+                         "events"
+                         "fill"
+                         "fit"
+                         "fixed"
+                         "font"
+                         "fonts"
+                         "gradient"
+                         "group"
+                         "image"
+                         "images"
+                         "in"
+                         "inherit"
+                         "item"
+                         "max"
+                         "middle"
+                         "min"
+                         "mouse_events"
+                         "name"
+                         "normal"
+                         "offset"
+                         "origin"
+                         "part"
+                         "parts"
+                         "pointer_mode"
+                         "precise_is_inside"
+                         "program"
+                         "programs"
+                         "rel1"
+                         "rel2"
+                         "relative"
+                         "repeat_events"
+                         "signal"
+                         "size"
+                         "smooth"
+                         "source"
+                         "spectra"
+                         "spectrum"
+                         "spread"
+                         "state"
+                         "step"
+                         "style"
+                         "styles"
+                         "tag"
+                         "target"
+                         "text"
+                         "text_class"
+                         "text_source"
+                         "to"
+                         "to_x"
+                         "to_y"
+                         "transition"
+                         "tween"
+                         "type"
+                         "use_alternate_font_metrics"
+                         "visible"
+                         "x"
+                         "y"
+                         ) t) "\\>[ \t]*[:,]")
+              '(2 font-lock-keyword-face))
+             )))
+  "Minor keywords")
+
+(defconst edje-font-lock-keywords-3
+  (eval-when-compile
+    (append edje-font-lock-keywords-2
             (list
              (list
               (concat "\\<"
@@ -187,12 +223,53 @@
              )))
   "Enumerate values")
 
-(defvar edje-font-lock-keywords edje-font-lock-keywords-2)
+(defconst edje-font-lock-keywords-4
+  (eval-when-compile
+    (append edje-font-lock-keywords-3
+            (list
+             (list
+              (concat "[ \t]*#"
+                      (regexp-opt
+                       '("if"
+                         "ifdef"
+                         "ifndef"
+                         "define"
+                         "else"
+                         "endif"
+                         "include"
+                         "undef") t) "[ \t]*")
+              '(1 font-lock-builtin-face))
+             )))
+  "CPP directives")
+
+(defconst edje-font-lock-keywords-5
+  (eval-when-compile
+    (append edje-font-lock-keywords-4
+            (list
+             (list "[ \t]*#undef[ \t]+\\([a-zA-Z_][a-zA-Z0-9_]*\\)"
+                   '(1 font-lock-variable-name-face))
+             (list "[ \t]*#define[ \t]+\\([a-zA-Z_][a-zA-Z0-9_]*\\)[ \t]*("
+                   '(1 font-lock-function-name-face))
+             (list "[ \t]*#define[ \t]+\\([a-zA-Z_][a-zA-Z0-9_]*\\)"
+                   '(1 font-lock-variable-name-face))
+             )))
+  "CPP directives that define constants")
+
+
+(defvar edje-font-lock-keywords edje-font-lock-keywords-5)
+
+(defvar edje-mode-syntax-table
+  (let ((edje-mode-syntax-table (make-syntax-table)))
+    ; This is added so entity names with underscores can be more easily parsed
+    (modify-syntax-entry ?_ "w" edje-mode-syntax-table)
+    edje-mode-syntax-table)
+  "Syntax table for edje-mode")
 
 (define-derived-mode edje-mode c-mode "Edje"
   "Major mode for editing Edje files"
   (interactive)
   (use-local-map edje-mode-map)
+  (set-syntax-table edje-mode-syntax-table)
   (set (make-local-variable 'font-lock-defaults) '(edje-font-lock-keywords))
   (set (make-local-variable 'require-final-newline) t)
   (run-hooks 'edje-mode-hook)
